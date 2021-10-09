4445df939527d9caf2c7c4b3a9fe986e5572ede9
Janek <jan.skiba.poczta@gmail.com>
2021-10-09 13:19:35 +0200

    add and style addRecipeForm

--- src/app/recipes/add-recipe/add-recipe.component.ts
+++ src/app/recipes/add-recipe/add-recipe.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
-  styleUrls: ['./add-recipe.component.scss']
+  styleUrls: ['./add-recipe.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddRecipeComponent implements OnInit {
+
+  addRecipeForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(80),
+    ]),
+    preparationTime: new FormControl('', [
+      Validators.required,
+    ]),
+    description: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(15),
+        Validators.maxLength(225),
+      ]),
+    ingredientName: new FormControl('', [
+      Validators.required,
+    ]),
+    ingredientQuantity: new FormControl('', [
+      Validators.required,
+    ])
+  });
+
+  ingredientsArray: Ingredient[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  resetForm() {
+    this.addRecipeForm.reset();
+  }
+
+  addIngredient(name: string, quantity: string) {
+    const ingredient: Ingredient = {
+      name: name,
+      quantity: quantity
+    };
+
+    this.ingredientsArray.push(ingredient);
+
+    this.addRecipeForm.get("ingredientName")?.reset();
+    this.addRecipeForm.get("ingredientQuantity")?.reset();
   }
 
 }
